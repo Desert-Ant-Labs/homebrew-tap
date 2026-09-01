7f90681831597393c681a66036edc0033647522f
Paul Veugen <paul@desertant.com>
2026-09-01 18:28:37 +0200

    Clipper 1.0.3

--- Casks/clipper.rb
+++ Casks/clipper.rb
@@ -1,6 +1,6 @@
 cask "clipper" do
-  version "1.0.2"
-  sha256 "090f38487330bfebfdad8cbf0a6440623d30bf5cf9794ff5c534efd5d959f7e4"
+  version "1.0.3"
+  sha256 "876fc1f2a00c3a59cad96b14d61d4b51a22d3b525cf834571936d6e0faa6f8e4"
 
   url "https://github.com/Desert-Ant-Labs/demo-clipper/releases/download/v#{version}/Clipper-#{version}.dmg",
       verified: "github.com/Desert-Ant-Labs/demo-clipper/"
